8a981bd208cfcc2a256567838867abcdb8a69bc1
Faultz// <115576970+PredatorDefi@users.noreply.github.com>
2025-05-06 19:19:39 +0200

    Update Notifications.tsx

--- src/Notifications.tsx
+++ src/Notifications.tsx
@@ -41,7 +41,7 @@ export const ToastProvider = ({ children }: ToastProviderProps) => {
   return (
     <ToastContext.Provider value={{ showToast }}>
       {children}
-      <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-2">
+      <div className="fixed bottom-4 right-4 z-9999999999999990 flex flex-col gap-2">
         {toasts.map(toast => (
           <div
             key={toast.id}
